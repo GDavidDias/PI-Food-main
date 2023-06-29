d189f1073f06317eb1c61259fd6fad8727db8517
GDavidDiasgit config --global user.name GDavidDias <guillermo.david.dias@gmail.com>
2023-06-29 19:49:50 -0300

    paginacion personalizada -gdd

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,23 +2,67 @@ import { useDispatch, useSelector } from "react-redux";
 import style from "./Home.module.css";
 import * as actions from "../../redux/actions";
 import Cards from "../cards/Cards";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 function Home(){
     //TRAIGO LOS ESTADOS GLOBALES
     const recipes = useSelector((state)=>state.filterRecipes);
+
+    //ESTADO LOCAL PARA PAGINADO
+    const ITEMS_PER_PAGE = 9;
+    const [pageItems, setPageItems] = useState(9);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [item, setItems] = useState([...recipes].splice(0,pageItems));
     
+    useEffect(()=>{
+        setItems([...recipes].splice(0,pageItems));
+    },[recipes]);
+
+    const nextPage = () => {
+        const next_page = currentPage +1;
+        const firstIndex = next_page * pageItems;
+        if(firstIndex>=recipes.length) return;
+        setItems([...recipes].splice(firstIndex,pageItems));
+        setCurrentPage(next_page);
+    }
+
+    const prevPage = () => {
+        const prev_page = currentPage-1;
+        const firstIndex = prev_page * pageItems;
+        if(prev_page<0) return;
+        setItems([...recipes].splice(firstIndex,pageItems));
+        setCurrentPage(prev_page);
+    };
+
+    const handleChange = (event) =>{
+        const {name, value} = event.target;
+        console.log("que trae itempagina: ",value)
+        setPageItems(value);
+    };
+
     const dispatch = useDispatch();
-    
+
     useEffect(()=>{
         dispatch(actions.addAllRecipes());
         dispatch(actions.addAllDiets());
-    },[])
+    },[pageItems])
 
     return(
         <div className={style.container}>
-            <Cards recipes={recipes} />
+            <div className={style.navPage}>
+                <button onClick={()=>prevPage()}>Prev</button>
+                <button onClick={()=>nextPage()}>Next</button>
+                <input 
+                    name="numberItem" 
+                    type="number" 
+                    value={pageItems}
+                    onChange={handleChange}
+                    ></input>
+            </div>
+            <div className={style.cardsContainer}>
+                <Cards recipes={item} />
+            </div>
         </div>
     )
 };
